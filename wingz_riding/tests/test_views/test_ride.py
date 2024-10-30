f96f2305769e59a2a2de7d826133fc19483f92e3
zhoujingryan <jingranzhou@126.com>
2024-10-30 23:58:07 +0800

    test: add more tests for rides list

--- wingz_riding/tests/test_views/test_ride.py
+++ wingz_riding/tests/test_views/test_ride.py
@@ -1,6 +1,9 @@
 from django.urls import reverse
+from django_fakery.faker_factory import factory
 
 from wingz.restframework.tests import BaseAPITestCase
+from wingz_riding.constants import RideStatus
+from wingz_riding.models import Ride
 from wingz_sso.constants import UserRole
 from wingz_sso.models import User
 
@@ -25,3 +28,52 @@ class RideViewSetAdminTestCase(BaseAPITestCase):
         assert response.status_code == 200
         data = response.data
         assert "count" in data
+
+    def test_filter_status_rides_list(self):
+        factory.m(Ride, quantity=3)(status=RideStatus.PICKUP)
+        factory.m(Ride, quantity=2)(status=RideStatus.EN_ROUTE)
+        factory.m(Ride, quantity=1)(status=RideStatus.DROPOFF)
+        url = reverse("riding-rides-list") + f"?status={RideStatus.PICKUP}"
+        response = self.client.get(url)
+        assert response.status_code == 200
+        data = response.data
+        assert data["count"] == 3
+
+        url = reverse("riding-rides-list") + f"?status={RideStatus.EN_ROUTE}"
+        response = self.client.get(url)
+        assert response.status_code == 200
+        data = response.data
+        assert data["count"] == 2
+
+        url = reverse("riding-rides-list") + f"?status={RideStatus.DROPOFF}"
+        response = self.client.get(url)
+        assert response.status_code == 200
+        data = response.data
+        assert data["count"] == 1
+
+    def test_filter_rider_email_rides_list(self):
+        u1 = factory.m(User)(email="test-rider1@email.com")
+        u2 = factory.m(User)(email="test-rider2@email.com")
+        u3 = factory.m(User)(email="example@email.com")
+
+        factory.m(Ride, quantity=3)(rider=u1)
+        factory.m(Ride, quantity=4)(rider=u2)
+        factory.m(Ride, quantity=5)(rider=u3)
+
+        url = reverse("riding-rides-list") + "?rider__email__icontains=Email"
+        response = self.client.get(url)
+        assert response.status_code == 200
+        data = response.data
+        assert data["count"] == 12
+
+        url = reverse("riding-rides-list") + "?rider__email=example@email.com"
+        response = self.client.get(url)
+        assert response.status_code == 200
+        data = response.data
+        assert data["count"] == 5
+
+        url = reverse("riding-rides-list") + "?rider__email__icontains=rider"
+        response = self.client.get(url)
+        assert response.status_code == 200
+        data = response.data
+        assert data["count"] == 7
