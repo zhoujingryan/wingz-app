af5e016a596473bad2cc5980dbc11fc9ac75fb51
zhoujingryan <jingranzhou@126.com>
2024-10-30 16:01:25 +0800

    feat: add sso app & add user model

---
+++ wingz_sso/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WingzSsoConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "wingz_sso"
---
+++ wingz_sso/models/user.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email, and password.
+        """
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("role", User.UserRole.REGULAR)
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+        db_table = "sso_user"
+
+    class UserRole(models.TextChoices):
+        ADMIN = "admin", "Admin"
+        REGULAR = "regular_user", "Regular User"
+
+    id_user = models.BigAutoField(primary_key=True)
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    role = models.CharField(
+        max_length=16, choices=UserRole.choices, default=UserRole.REGULAR
+    )
+    phone_number = models.CharField(max_length=32, null=True)
+
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
