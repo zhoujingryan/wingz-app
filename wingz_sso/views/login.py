3a593b91e87e9d236b88d705a31978dedc623421
zhoujingryan <jingranzhou@126.com>
2024-10-30 18:47:47 +0800

    feat: user login api response add refresh and access field

--- wingz_sso/views/login.py
+++ wingz_sso/views/login.py
@@ -1,14 +1,20 @@
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework_simplejwt.serializers import TokenObtainSerializer
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class PasswordLoginResponseSerializer(serializers.Serializer):
+    access = serializers.CharField(label="access token")
+    refresh = serializers.CharField(label="refresh token")
 
 
 class PasswordLoginAPIView(TokenObtainPairView):
     permission_classes = ()
-    serializer_class = TokenObtainSerializer
+    serializer_class = TokenObtainPairSerializer
 
     @swagger_auto_schema(
-        responses={200: TokenObtainSerializer()},
+        responses={200: PasswordLoginResponseSerializer()},
         operation_summary="user password login",
     )
     def post(self, request, *args, **kwargs):
